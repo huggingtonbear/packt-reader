33f7994d87f9d1a3df9b440bc6a7d8d8aeae69e1
huggingtonbear <oli.huggins@gmail.com>
2016-08-23 11:02:39 +0100

    Updated Library Screen to include search filter

--- js/sidenav-accordion.js
+++ js/sidenav-accordion.js
@@ -3,4 +3,10 @@
     actives.each( function (index, element) {
         $(element).collapse('hide');
     })
+});
+      $('#filters .collapse').on('show.bs.collapse', function (e) {
+	var actives = $('#filters').find('.in, .collapsing');
+	actives.each( function (index, element) {
+	    $(element).collapse('hide');
+	})
 })
