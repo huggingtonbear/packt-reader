deecbb5c0a4788fbe2e0799bb9a2568ea54ae85c
supachris28 <keych.ck@gmail.com>
2017-01-06 15:24:57 +0000

    Fixed book launching

--- js/custom-angular.js
+++ js/custom-angular.js
@@ -51,12 +51,12 @@ maptModule.directive('maptBook', function () {
         , template: '\
                 <div class="book-wrapper col-xs-6 col-sm-3 col-lg-2 "> \
                     <div class="text-center book"> \
-                        <a href="index.html"><img class="img-responsive title-shadow" src="{{coverImage}}" title="{{bookTitle}}"></a> \
+                        <a href="index.html" target="_blank"><img class="img-responsive title-shadow" src="{{coverImage}}" title="{{bookTitle}}"></a> \
                         <div ng-show="progress" class="progress mt10 mb15"> \
                             <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100" style="width: {{progress}};"> <span class="sr-only">30% Complete</span> </div> \
                         </div> \
                         <button class="btn btn-primary mt5 btn-book btn-book-1" type="button" onclick="$(this).find(\'i\').toggleClass(\'fa-plus fa-minus\');$(this).toggleClass(\'btn-primary btn-danger\')" ng-click="addAlert()"><i class="fa fa-plus"></i></button> \
-                        <button class="btn btn-primary mt5 btn-book btn-book-2" type="button"><i class="fa fa-play"></i></button> \
+                        <a class="btn btn-primary mt5 btn-book btn-book-2" type="button" href="index.html" target="_blank"><i class="fa fa-play"></i></button> \
                     </div> \
                 </div>'
     };
